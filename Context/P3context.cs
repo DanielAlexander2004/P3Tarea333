1acff70ada268f7636522a415a6f8cd02cc01804
DanielAlexander2004 <danielalexand09@outlook.es>
2025-08-01 22:44:12 -0400

    agregando create

---
+++ Context/P3context.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using P3tarea333;
+
+namespace P3Tarea3.Context
+{
+	public class P3Context : DbContext
+	{
+
+		public P3Context(DbContextOptions<P3Context> db) : base(db)
+		{
+
+
+		}
+
+		public DbSet<Citas> Citas { get; set; }
+	}
+}
---
+++ Services/CitasServices.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using P3Tarea3.Context;
+using P3Tarea3.Interfaces;
+using P3tarea333;
+
+namespace P3Tarea3.Services
+{
+	public class CitasServices : ICitas
+	{
+		private readonly P3Context context;
+		public CitasServices(P3Context Context)
+		{
+			this.context = Context;
+		}
+		public string SetCitas(Citas model)
+		{
+			context.Citas.Add(model);
+			context.SaveChanges();
+			return "Registro insertado";
+		}
+	}
+}
